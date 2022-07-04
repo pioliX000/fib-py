d6d90290c469c2fff434a6aea850d5f560f945eb
pioliX000 <65280877+pioliX000@users.noreply.github.com>
2022-07-04 07:46:28 +0200

    Update fib.py

--- fib.py
+++ fib.py
@@ -4,17 +4,16 @@ x = 0
 y = 1
 
 i = 0
-p = input("How many iterations: ")
+p = int(input("How many iterations: "))
 
-while i < int(p):
-	i = i + 1
+while i < p:
+        i = i + 1
 
-	time.sleep(0.1)	
-	print(f"{x} iteration {i}")
+        time.sleep(0.1)
+        print(f"{x} iteration {i}")
 
-	z = x + y
-	x = y	
-	y = z
+        z = x + y
+        x = y
+        y = z
 
 print("Tada!")
-
