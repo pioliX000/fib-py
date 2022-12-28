284f220c280539eccdbd0d5f617a9c4273e35df0
pioliX000 <65280877+pioliX000@users.noreply.github.com>
2022-12-28 03:51:28 +0100

    Update fib.py

--- fib.py
+++ fib.py
@@ -3,12 +3,9 @@ import time
 x = 0
 y = 1
 
-i = 0
 p = int(input("How many iterations: "))
 
-while i < p:
-        i += 1
-
+for i in range(0, p):
         time.sleep(0.1)
         print(f"{x} iteration {i}")
 
